53440ef3afc3cdbc3d1d6db71e412e12a272c6a8
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 01:21:54 +0300

    module 3. task 4. fix

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,13 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c = 'c';
-        char h = 'h';
-        char a = 'a';
-        char r = 'r';
-        System.out.println((int)c);
-        System.out.println((int)h);
-        System.out.println((int)a);
-        System.out.println((int)r);
+        int charC = (int) 'c';
+        int charH = (int) 'h';
+        int charA = (int) 'a';
+        int charR = (int) 'r';
+
+        System.out.println(charC);
+        System.out.println(charH);
+        System.out.println(charA);
+        System.out.println(charR);
     }
 }
